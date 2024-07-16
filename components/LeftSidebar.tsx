73391f204b0cf0803c84d5d40b864be387c554ec
Zakry27 <zak90078@gmail.com>
2024-07-16 21:32:24 +0100

    Commit #5 implementation of Convex

--- components/LeftSidebar.tsx
+++ components/LeftSidebar.tsx
@@ -1,9 +1,16 @@
+'use client';
+
 import { sidebarLinks } from '@/constants'
+import { cn } from '@/lib/utils'
 import Image from 'next/image'
 import Link from 'next/link'
+import { usePathname, useRouter } from 'next/navigation'
 import React from 'react'
 
 const LeftSidebar = () => {
+    const pathname = usePathname();
+    const router = useRouter();
+
   return (
     <section className='left_sidebar'>
         <nav className='flex flex-col gap-6'>
@@ -11,8 +18,15 @@ const LeftSidebar = () => {
             <Image src="/icons/logo.svg" alt='logo' width={23} height={27}/>
             <h1 className='text-24 font-extrabold text-white max-lg:hidden'>Orbicast</h1>
             </Link>
-            {sidebarLinks.map(({route, label}) => {
-                return <Link href={route}>{label}</Link>
+            {sidebarLinks.map(({route, label, imgURL}) => {
+                const isActive = pathname === route || pathname.startsWith('${route}/');
+
+                return <Link href={route} key={label} className={cn("flex gap-3 items-center py-4 max-lg:px-4 justify-start", {
+                    'bg-nav-focus border-r-4 border-orange-1': isActive
+                })}>
+                    <Image src={imgURL} alt={label} width={24} height={24}/>
+                    <p>{label}</p>
+                </Link>
             })}
         </nav>
     </section>
