68671474721399fc4a693e19278754f282dfd940
Zakry27 <zak90078@gmail.com>
2024-07-17 22:18:07 +0100

    Commit #6 : Creation of podcast page

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+    providers: [
+      {
+        domain: "https://expert-squid-39.clerk.accounts.dev",
+        applicationID: "convex",
+      },
+    ]
+  };
+
+  export default authConfig;
