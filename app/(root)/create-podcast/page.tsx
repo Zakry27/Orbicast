2b9fe6580ea2b7c640ab3f92b120d67744435f6d
Zakry27 <zak90078@gmail.com>
2024-07-23 22:33:13 +0100

    Commit #7 : Generate podcast and OpenAI integration

--- app/(root)/create-podcast/page.tsx
+++ app/(root)/create-podcast/page.tsx
@@ -31,7 +31,7 @@ import GenerateThumbnail from "@/components/GenerateThumbnail"
 import { Loader } from "lucide-react"
 import { Id } from "@/convex/_generated/dataModel"
 
-const voiceCategories = ['allow', 'shimmer', 'nova', 'echo', 'fable', 'onyx'];
+const voiceCategories = ['alloy', 'shimmer', 'nova', 'echo', 'fable', 'onyx'];
 
 const formSchema = z.object({
   podcastTitle: z.string().min(2),
@@ -77,9 +77,9 @@ const CreatePodcast = () => {
               name="podcastTitle"
               render={({ field }) => (
                 <FormItem className="flex flex-col gap-2.5">
-                  <FormLabel className="text-16 font-bold text-white-1">Username</FormLabel>
+                  <FormLabel className="text-16 font-bold text-white-1">Title</FormLabel>
                   <FormControl>
-                    <Input className="input-class focus-visible:ring-orange-1" placeholder="JSM Pro Podcast" {...field} />
+                    <Input className="input-class focus-visible:ring-offset-orange-1" placeholder="JSM Pro Podcast" {...field} />
                   </FormControl>
                   <FormMessage className="text-white-1"/>
                 </FormItem>
@@ -91,10 +91,10 @@ const CreatePodcast = () => {
                 Select AI Voice
               </Label>
               <Select onValueChange={(value) => setVoiceType(value)}>
-                <SelectTrigger className={cn('text-16 w-full border-none bg-black-1 text-gray-1')}>
+                <SelectTrigger className={cn('text-16 w-full border-none bg-black-1 text-gray-1 focus-visible:ring-offset-orange-1')}>
                   <SelectValue placeholder="Select AI Voice" className="placeholder:text-gray-1"/>
                 </SelectTrigger>
-                <SelectContent className="text-16 border-none bg-black-1 font-bold text-white-1 focus:ring-orange-1">
+                <SelectContent className="text-16 border-none bg-black-1 font-bold text-white-1 focus-visible:ring-offset-orange-1">
                   {voiceCategories.map((category) => (
                     <SelectItem key={category} value={category} className="capitalize focus:bg-orange-1">
                       {category}
@@ -117,7 +117,7 @@ const CreatePodcast = () => {
                 <FormItem className="flex flex-col gap-2.5">
                   <FormLabel className="text-16 font-bold text-white-1">Description</FormLabel>
                   <FormControl>
-                    <Textarea className="input-class focus-visible:ring-orange-1" placeholder="Write a short descrition about the Podcast" {...field} />
+                    <Textarea className="input-class focus-visible:ring-offset-orange-1" placeholder="Write a short descrition about the Podcast" {...field} />
                   </FormControl>
                   <FormMessage className="text-white-1"/>
                 </FormItem>
@@ -129,7 +129,7 @@ const CreatePodcast = () => {
           <GeneratePodcast
             setAudioStorageId={setAudioStorageId}
             setAudio={setAudioUrl}
-            voiceType={voiceType}
+            voiceType={voiceType!}
             audio={audioUrl}
             voicePrompt={voicePrompt}
             setVoicePrompt={setVoicePrompt}
